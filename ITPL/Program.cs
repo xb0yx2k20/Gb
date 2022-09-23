6a82c4e36a0f5543585f529640428091ad6bb981
xb0yx2k20 <musraul@yandex.ru>
2022-09-23 23:22:35 +0300

    dz1

--- ITPL/Program.cs
+++ ITPL/Program.cs
@@ -1,9 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 2
+Console.WriteLine("Задача 2 - введите 2 числа, чтобы найти максимальное из них");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b) {
-    Console.WriteLine($"max {a}");
+Console.WriteLine($"max = {Math.Max(a, b)}");
+
+
+// Задача 4
+Console.WriteLine("Задача 4 - введите 3 числа, чтобы найти максимальное из них");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"max = {Math.Max(Math.Max(x, y), Math.Max(z, y))}");
+
+
+// Задача 6
+Console.WriteLine("Задача 6 - введите число, чтобы определить четность");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0) {
+    Console.WriteLine("дa");
 }
 else {
-    Console.WriteLine($"max {b}");
+    Console.WriteLine("нет");
+}
+
+// Задача 8
+Console.WriteLine("Задача 6 - введите число, чтобы найти четные числа, которые меньше введенного");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N != 0) {
+    if (N % 2 == 0) {
+        Console.Write($"{N} ");
+        N -= 2;
+        }
+    else {
+        Console.Write($"{N - 1} ");
+        N -= 1;
+    }
+    while (N >= 1) {
+        Console.Write($"{N} ");
+        N -= 2;
+    }
 }
