7bbe44b7c52ac650408cd312eca0b2eee65c1902
xb0yx2k20 <musraul@yandex.ru>
2022-09-23 23:00:53 +0300

    New folder

---
+++ ITPL/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) {
+    Console.WriteLine($"max {a}");
+}
+else {
+    Console.WriteLine($"max {b}");
+}
