25af419c0aad4362cbbbb4551ef0e465d9cdc6fd
xb0yx2k20 <musraul@yandex.ru>
2022-10-08 14:13:19 +0300

    HW6

--- ITPL/Program.cs
+++ ITPL/Program.cs
@@ -1,28 +1,21 @@
-﻿// Homework 5
+﻿// Homework 6
 
-int[] arr(int n) {
-    int[] nums = new int[n];
-    for (int i = 0; i < n; i++) {
-        nums[i] = new Random().Next(-100, 100);
-    }
-    return nums;
-}
-
-// Задача 34, 36, 38
-int[] mas = arr(5);
+// Задача 41
 int k = 0;
-int sum = 0;
-for (int i = 0; i < 5; i++) {
-    Console.Write($"{mas[i]} ");
-    if (mas[i] % 2 == 0) {
+int m = 5;
+for (int i = 0; i < m; i++){
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n > 0) {
         k++;
     }
-    if (i % 2 == 1) {
-        sum += mas[i];
-    }
-
 }
-Console.WriteLine($"\nTask_34 -> {k}");
-Console.WriteLine($"Task_36 -> {sum}");
-Console.WriteLine($"Task_38 -> {mas.Max() - mas.Min()}");
+
+// Задача 43
+int k1 = Convert.ToInt32(Console.ReadLine());
+int k2 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+int x = (b2-b1)/(k1 - k2);
+Console.WriteLine($"\nTask_41 -> {k}");
+Console.WriteLine($"Task_43 -> x: {x}, y: {b1 + k1 * x}");
 
