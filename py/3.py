da4ff315e8681aae526fa0c2f2e2ad4ff98342f4
xb0yx2k20 <musraul@yandex.ru>
2023-02-16 23:25:06 +0300

    dz5

--- py/3.py
+++ py/3.py
@@ -1,2 +1,76 @@
-a = -1
-print(a ** 2)
+import numpy as np
+
+def display(mas):
+    for i in range(3):
+        for j in range(3):
+            print(mas[i][j], end=" ") 
+            if (j+1) % 3 == 0: 
+                print()
+
+def check(mas):
+    for i in range(3):
+        if mas[0][i] == mas[1][i] == mas[2][i] and mas[0][i] != '-':
+            if mas[0][0] == 'X':
+                print("Игрок 2 победил")
+            else:
+                print("Игрок 1 победил")
+            return False
+        elif mas[i][0] == mas[i][1] == mas[i][2] and mas[i][0] != '-':
+            if mas[0][0] == 'X':
+                print("Игрок 2 победил")
+            else:
+                print("Игрок 1 победил")
+            return False
+    if mas[0][0] == mas[1][1] == mas[2][2] and mas[0][0] != '-':
+        if mas[0][0] == 'X':
+            print("Игрок 2 победил")
+        else:
+            print("Игрок 1 победил")
+            return False
+    elif mas[2][0] == mas[1][1] == mas[0][2] and mas[1][1] != '-':
+        if mas[0][0] == 'X':
+            print("Игрок 2 победил")
+        else:
+            print("Игрок 1 победил")
+            return False
+    else:
+        k = 0
+        for i in range(3):
+            for j in range(3):
+                if '-' == mas[i][j]:
+                    k += 1
+        if k == 0:
+            print("Ничья")
+            return False
+        return True
+
+
+mas = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])   
+res = [['-'] * 3 for i in range(3)]
+k = 0
+while check(res):
+    display(mas)
+    if k % 2 == 0:
+        print("Игрок 1, выбери куда нарисовать О")
+        n = int(input())
+        idx = np.where(mas == n)
+        if res[idx[0][0]][idx[1][0]] != '-':
+            print("Занято!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            k += 1
+        else:
+            res[idx[0][0]][idx[1][0]] = 'O'
+        display(res)
+    else:
+        print("Игрок 2, выбери куда нарисовать X")
+        n = int(input())
+        idx = np.where(mas == n)
+        if res[idx[0][0]][idx[1][0]] != '-':
+            print("Занято!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            k += 1
+        else:
+            res[idx[0][0]][idx[1][0]] = 'X'
+        display(res)
+    k += 1
+    print()
+    
+    
--- py/5.py
+++ py/5.py
@@ -1,37 +1,15 @@
-with open('file1.txt') as f1:
+with open('py/file1.txt') as f1:
     line1 = f1.readline()
-with open('file2.txt') as f2:
-    line2 = f2.readline()
-line1, line2 = line1.split(), line2.split()
-
-res, subRes = (), ()
-for i in range(2, len(line1), 4):
-    print(line1[i])
-    if line1[i] in line2 and line1[i] != '=':
-        res += (int(line1[i - 2]) + int(line2[line2.index(line1[i]) - 2])), line1[i]
+print(line1)
+k = 1
+res = ''
+for i in range(len(line1) - 1):
+    if line1[i] == line1[i + 1]:
+        k += 1
     else:
-        subRes += (int(line1[i - 2]), line1[i])
-print(res)
-if (len(res) // 10 - 1) != (len(line2) - 3) // 4:
-    for i in range(2, len(line2), 4):
-        if line2[i] not in res:
-            subRes += (int(line2[i - 2]), line2[i])
-
-res = list(subRes + res)[:-1]
-
-fl = 0
-for i in range(1, len(res) * 2 - 1, 2):
-    if fl == 0:
-        res.insert(i, ' * ')
-        fl = 1
-    else:
-        res.insert(i, '  +  ')
-        fl = 0
-res = [str(i) for i in res]
+        res += str(k) + line1[i]
+        k = 1
+res += str(k) + line1[-1]
 print(res)
 with open('res.txt', 'w') as result:
-    for i in res:
-        print(i, end='')
-        result.write(i)
-    print(" = 0")
-    result.write(" = 0")
+    result.write(res)
