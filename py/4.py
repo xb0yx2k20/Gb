9509b8d9ddc93c74f1cf26a1fb665e261c7cb55e
xb0yx2k20 <musraul@yandex.ru>
2023-02-16 23:28:58 +0300

    smallFix

--- py/4.py
+++ py/4.py
@@ -1,14 +1,15 @@
-from random import randint
-
-k = int(input())
-koefs = [randint(0, 100) for i in range(k + 1)]
-res = ["{kf} * x^{ind}".format(kf = koefs[i], ind = i) for i in range(k, 0, -1) if koefs[i] != 0]
-
-if koefs[0] != 0: 
-    res.append(koefs[0])
-for i in range(1, len(res) * 2 - 1, 2):
-    res.insert(i, '  +  ')
-
-for i in res:
-    print(i, end='')
-print(" = 0")
+with open('py/file1.txt') as f1:
+    line1 = f1.readline()
+print(line1)
+k = 1
+res = ''
+for i in range(len(line1) - 1):
+    if line1[i] == line1[i + 1]:
+        k += 1
+    else:
+        res += str(k) + line1[i]
+        k = 1
+res += str(k) + line1[-1]
+print(res)
+with open('res.txt', 'w') as result:
+    result.write(res)
