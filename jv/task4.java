8c33bec5280200a9d413e6dc95bad05e954da6e4
xb0yx2k20 <musraul@yandex.ru>
2023-03-06 21:19:16 +0300

    javaDZ1

--- jv/task4.java
+++ jv/task4.java
@@ -21,12 +21,16 @@ public class task4 {
                     array[i][j] = (array[2][j] - array[1][j] - array[0][j] + 111);
                     if ('0' >= array[i][j] || '9' <= array[i][j]) {
                         System.out.println("No solution");
-                        System.exit();
+                        System.exit(0);
                     }
                 }
-                System.out.print((char)array[i][j] + " ");
+                System.out.print((char)array[i][j]);
             }
-            System.out.println();
+            if (i == 0) {
+                System.out.print(" + ");
+            } else if (i != 2) {
+                System.out.print(" = ");
+            }
         }
     }
 }
