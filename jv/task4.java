706bd56a07a64e9e207158fae02ad35f3653f817
xb0yx2k20 <musraul@yandex.ru>
2023-03-11 12:15:46 +0300

    lptopChanges

--- jv/task4.java
+++ jv/task4.java
@@ -10,16 +10,24 @@ public class task4 {
         String[] arr = str.split(" ");
 
         int[][] array = new int[3][2];
+        
         for (int i = 0; i < arr.length; i+=2) {
             char[] chars = arr[i].toCharArray();
             array[i / 2][0] = chars[0];
             array[i / 2][1] = chars[1];
         }
+        int fl = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 2; j++) {
-                if (array[i][j] == 63) {
-                    array[i][j] = (array[2][j] - array[1][j] - array[0][j] + 111);
-                    if ('0' >= array[i][j] || '9' <= array[i][j]) {
+                if (array[i][j] == '?') {
+                    if ((array[2][j] - array[1][j] - array[0][j] + 63) < 0)  {
+                        array[i][j] = (array[2][j] - array[1][j] - array[0][j] + 111 + 10);
+                        fl += 1;
+                    } else {
+                        array[i][j] = (array[2][j] - array[1][j] - array[0][j] + 111 - fl);
+                        //System.out.println("\n--------" + array[i][j]);
+                    }
+                    if ('0' > array[i][j] || '9' < array[i][j]) {
                         System.out.println("No solution");
                         System.exit(0);
                     }
